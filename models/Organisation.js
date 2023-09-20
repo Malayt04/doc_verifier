d8faa661dd4c4dc699e951d1d37d065022ed537e
Malay Tiwari <malayt04@gmail.com>
2023-09-20 16:41:15 +0530

    "Sixth commit"

--- models/Organisation.js
+++ models/Organisation.js
@@ -3,37 +3,8 @@ const {isEmail}=require('validator');
 const bcrypt=require('bcrypt');
 
 
-
-const orgSchema=new mongoose.Schema({
-
-name:{
-    type:String,
-    required:[true,'Please enter  name ot your organisation. '],
-    minlength:[3,'Name of the organisation should be longer than 3 characters']
-}
-,
-email:{
-type:String,
-required:[true,'Please enter a valid email address'],
-unique:true,
-lowercase:true,
-validate:[isEmail,'Please enter a valid email address']
-}
-,
-password:{
-    type:String,
-    required:[true,'Please enter a valid password'],
-    minlength:[8,'Password must be at least 8 characters']
-},
-
-username:{
-    type:String,
-    unique:true,
-    required:[true,'Please enter a username'],
-    minlength:[3,'Username of the organisation should be longer than 3 characters']
-},
-regestiredUsers:[
-    {
+const regUsers=mongoose.Schema({
+    
 
         fullName:{
             type:String,
@@ -57,14 +28,42 @@ regestiredUsers:[
         type:String,
         required:[true,'Please enter a valid name']
         }
-        ,
-    hashedPassword:{
-        type:String
-    }
-    
-    }
 
-],
+    
+    
+})
+
+
+
+
+
+const orgSchema=new mongoose.Schema({
+
+name:{
+    type:String,
+    required:[true,'Please enter  name ot your organisation. '],
+    minlength:[3,'Name of the organisation should be longer than 3 characters']
+}
+,
+email:{
+type:String,
+required:[true,'Please enter a valid email address'],
+lowercase:true,
+validate:[isEmail,'Please enter a valid email address']
+}
+,
+password: {
+    type: String,
+    required: [true, 'Please enter a valid password']
+},
+
+username:{
+    type:String,
+    unique:true,
+    required:[true,'Please enter a username'],
+    minlength:[3,'Username of the organisation should be longer than 3 characters']
+},
+regestiredUsers:[regUsers],
 
 history:[]
 });
